e075cbe875db5b5b648d63101fca86632071a69c
Afia Rahman <rahmanmou26@gmail.com>
2024-04-26 12:55:47 +0600

    first commit

---
+++ src/Layouts/MainLayout.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+
+const MainLayout = () => {
+    return (
+        <div className="font-rale">
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default MainLayout;
